d80d359b3ef144711973c3b415dc2f2e6fc696ee
Yuta Saito <kateinoigakukun@gmail.com>
2022-01-29 19:39:52 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,26 +1,2 @@
-// Borrowed from https://dev.to/stefnotch/enabling-coop-coep-without-touching-the-server-2d3n
-self.addEventListener("install", function() {
-    self.skipWaiting();
-});
-self.addEventListener("activate", (event)=>{
-    event.waitUntil(self.clients.claim());
-});
-self.addEventListener("fetch", function(event) {
-    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
-    event.respondWith(fetch(event.request).then(function(response) {
-        if (!response.url.includes("index.html")) return response;
-        const newHeaders = new Headers(response.headers);
-        newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
-        newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-        const moddedResponse = new Response(response.body, {
-            status: response.status,
-            statusText: response.statusText,
-            headers: newHeaders
-        });
-        return moddedResponse;
-    }).catch(function(e) {
-        console.error(e);
-    }));
-});
-
+self.addEventListener("install",(function(){self.skipWaiting()})),self.addEventListener("activate",(e=>{e.waitUntil(self.clients.claim())})),self.addEventListener("fetch",(function(e){"only-if-cached"===e.request.cache&&"same-origin"!==e.request.mode||e.respondWith(fetch(e.request).then((function(e){const t=new Headers(e.headers);t.set("Cross-Origin-Embedder-Policy","require-corp"),t.set("Cross-Origin-Opener-Policy","same-origin");return new Response(e.body,{status:e.status,statusText:e.statusText,headers:t})})).catch((function(e){console.error(e)})))}));
 //# sourceMappingURL=sw.js.map
